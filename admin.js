9d60a390b8be1d7a57990f8465558c0532729cb6
obsessson <khomenkomargoo@gmail.com>
2025-12-13 19:35:33 +0200

    55

--- admin.js
+++ admin.js
@@ -18,7 +18,7 @@ productForm.addEventListener('submit', function(event){
     }
     });
 
-    xhr.open("POST", "https://market-6d33.restdb.io/rest/products");
+    xhr.open("POST", "https://market-c850.restdb.io/rest/product");
     xhr.setRequestHeader("content-type", "application/json");
     xhr.setRequestHeader("x-apikey", "61b3ca4f72a03f5dae8222ad");
     xhr.setRequestHeader("cache-control", "no-cache");
